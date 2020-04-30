93f1b89d3b4161a1d9f7de0237ed033b3b28993d
Jesper Kristensen <cableman@linuxdev.dk>
2020-04-30 17:24:38 +0200

    DDBTEAM-295: Use input parameter in gen script

--- generate.sh
+++ generate.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-java -DapiTests=false -DmodelTests=false -jar /usr/local/Cellar/swagger-codegen/3.0.19/libexec/swagger-codegen-cli.jar generate -i ../ddb-cover-service-upload/public/openapi/openapi.yaml -l php --config config.json --output "."
+java -DapiTests=false -DmodelTests=false -jar /usr/local/Cellar/swagger-codegen/3.0.19/libexec/swagger-codegen-cli.jar generate -i $1 -l php --config config.json --output "."
 
 rm -rf .swagger-codegen
