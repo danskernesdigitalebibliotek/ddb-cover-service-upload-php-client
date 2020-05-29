9a7338d83a79d08c3f0b529b6e8b27ff2b073c63
Jesper Kristensen <cableman@linuxdev.dk>
2020-05-29 11:01:02 +0200

    DDBTEAM-498: Updated swagger path in sh file

--- generate.sh
+++ generate.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-java -DapiTests=false -DmodelTests=false -jar /usr/local/Cellar/swagger-codegen/3.0.20/libexec/swagger-codegen-cli.jar generate -i $1 -l php --config config.json --output "."
+java -DapiTests=false -DmodelTests=false -jar /usr/local/opt/swagger-codegen/libexec/swagger-codegen-cli.jar generate -i $1 -l php --config config.json --output "."
 
 rm -rf .swagger-codegen
